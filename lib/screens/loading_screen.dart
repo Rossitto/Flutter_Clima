c045ee25bab60919ddbe7676a4d27067678188f6
Rossitto <lucas74rossi@gmail.com>
2020-06-02 21:59:47 -0300

    refactored code to properly files separating in services

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+import 'package:clima/services/networking.dart';
 
 // const apiKey = '2bcb78674d978b3f2eb2316a0381a85f';
 const apiKey = '361da820191544b57613ad536d4e8046';
@@ -18,38 +17,20 @@ class _LoadingScreenState extends State<LoadingScreen> {
   @override
   void initState() {
     super.initState();
-    getLocation();
+    getLocationData();
   }
 
-  void getLocation() async {
+  void getLocationData() async {
     Location location = Location();
     await location.getCurrentLocation();
 
     latitude = location.latitude;
     longitude = location.longitude;
 
-    getData();
-  }
-
-  void getData() async {
-    http.Response response = await http.get(
+    NetworkHelper networkHelper = NetworkHelper(
         'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
 
-    if (response.statusCode == 200) {
-      String data = response.body;
-
-      var decodedData = jsonDecode(data);
-
-      double temperature = decodedData['main']['temp'];
-      int conditionNumber = decodedData['weather'][0]['id'];
-      String cityName = decodedData['name'];
-
-      print(temperature);
-      print(conditionNumber);
-      print(cityName);
-    } else {
-      print(response.statusCode);
-    }
+    var weatherData = networkHelper.getData();
   }
 
   @override
